698e7a97b40980ce2403aa40830638186c3e1f2d
NicolasGoulart18 <nicolasgoulartt2008@gmail.com>
2026-09-12 20:51:40 -0300

    feat: add transaction registration

--- src/main/java/br/com/nicolas/financemanager/Main.java
+++ src/main/java/br/com/nicolas/financemanager/Main.java
@@ -1,10 +1,12 @@
 package br.com.nicolas.financemanager;
 import br.com.nicolas.financemanager.model.TransactionType;
+import br.com.nicolas.financemanager.service.FinanceManager;
 import br.com.nicolas.financemanager.model.Transaction;
 import java.math.BigDecimal;
 public class Main {
     public static void main(String[] args) {
-        Transaction salary= new Transaction("salario", new BigDecimal(2500), TransactionType.INCOME);
+        FinanceManager manager=new FinanceManager();
+        Transaction salary= new Transaction("salario", new BigDecimal("2500.00"), TransactionType.INCOME);
         Transaction netflix = new Transaction("netflix", new BigDecimal("50.00"), TransactionType.EXPENSE);
         System.out.println(salary.getDescription());
         System.out.println(salary.getValue());
@@ -12,6 +14,7 @@ public class Main {
         System.out.println(salary.isExpense());
         System.out.println(netflix.getSignedValue());
         System.out.println(netflix);
-        
+        manager.addTransaction(salary);
+        manager.addTransaction(netflix);
     }
 }
