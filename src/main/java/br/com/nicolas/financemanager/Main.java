91f52c5adf79579a1933bfe9fbe0965c4434e998
NicolasGoulart18 <nicolasgoulartt2008@gmail.com>
2026-09-12 13:30:57 -0300

    feat: create first transaction example

--- src/main/java/br/com/nicolas/financemanager/Main.java
+++ src/main/java/br/com/nicolas/financemanager/Main.java
@@ -1,7 +1,12 @@
 package br.com.nicolas.financemanager;
-
+import br.com.nicolas.financemanager.model.TransactionType;
+import br.com.nicolas.financemanager.model.Transaction;
+import java.math.BigDecimal;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("=== Finance Manager ===");
+        Transaction salary= new Transaction("salario", new BigDecimal(2500), TransactionType.INCOME);
+        System.out.println(salary.getDescription());
+        System.out.println(salary.getValue());
+        System.out.println(salary.getType());
     }
 }
